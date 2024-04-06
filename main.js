2470aef26ac0b6560d76c158cccefa148d52130b
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-04-06 17:57:54 +0300

    init

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-function greeting() {
-  console.log("Hello World!");
-}
+let x;
+let y;
 
-greeting();
+const getPos  = () => navigator.geolocation.getCurrentPosition((pos) => {
+    x = pos.coords.latitude;
+    y = pos.coords.longitude;
+    map.setView([x, y], 13)
+})
+
+const map = L.map('map', {zoomControl: false}).setView([60, 24], 10);
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    ext: 'png'
+}).addTo(map);
+
+getPos();
