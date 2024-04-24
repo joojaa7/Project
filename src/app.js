585746d40cef1489930b8a9217b0b6e5acbd009c
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-04-25 02:48:20 +0300

    m

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import api from './api/index.js';
-import path from 'path';
-import {fileURLToPath} from 'url';
 
 const app = express();
+const cors = require('cors');
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
