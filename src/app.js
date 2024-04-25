d472e13e6fd18ea511fe54dbe5242a6debc38686
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-04-25 03:44:29 +0300

    m

--- src/app.js
+++ src/app.js
@@ -11,14 +11,16 @@ app.use(express.urlencoded({extended: true}));
 app.use('/restaurant/', express.static('Code'));
 app.use('/restaurant/', express.static('uploads'));
 
-app.use((function(req, res, next) {
+app.use('/restaurant/', api);
+app.get('/restaurant/get', (req, res) => {
+  res.send('Welcome to my REST API!');
+});
+
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin (you can restrict this to specific origins)
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH, OPTIONS");
   next();
-}), '/restaurant/', api);
-app.get('/restaurant/get', (req, res) => {
-  res.send('Welcome to my REST API!');
 });
 
 
