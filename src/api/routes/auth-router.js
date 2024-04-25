1e7a5ed814dc4a4c9bc36fba4af6fbeea6c77b22
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-04-25 21:08:08 +0300

    m

--- src/api/routes/auth-router.js
+++ src/api/routes/auth-router.js
@@ -8,7 +8,7 @@ const authRouter = express.Router();
 authRouter.use(cors());
 
 authRouter.route('/').get((req, res, next) => {
-  console.log(req.body)
+  console.log(req.headers)
   console.log('Get catch')
   const user = {
     username: 'test',
