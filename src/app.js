c57389c6493db55da4f6acc91bd9218a498094a1
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-04-25 10:27:42 +0300

    m

--- src/app.js
+++ src/app.js
@@ -13,6 +13,11 @@ app.use('/restaurant/', express.static('uploads'));
 
 app.use('/restaurant/', api);
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 export default app;
