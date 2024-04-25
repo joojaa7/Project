ce5b495db73ba0561461a43843ea6055904f28a0
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-04-25 11:21:51 +0300

    m

--- src/app.js
+++ src/app.js
@@ -13,6 +13,13 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 //app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
