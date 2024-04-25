7841a0cd61b9be0f9c3852633b4c6c8b552eeade
Joojaa7 <mikael.asikainen@metropolia.fi>
2024-04-25 21:10:17 +0300

    m

--- Code/main.js
+++ Code/main.js
@@ -383,6 +383,7 @@ const register = () => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify(userData)
     }
